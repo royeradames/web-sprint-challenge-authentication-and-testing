d91bdc5231bf0c6fc2c036acd1565a4ae4ae0133
royeradames <royeraadames@gmail.com>
2020-09-18 12:28:22 -0400

    complete v1 of authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -4,5 +4,25 @@
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  console.log(`inside restricted-middleware`)
+  const token = req.headers.authorization;
+  const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      console.log(`inside verify`)
+      if (err) {
+        console.log(`inside if`)
+
+        res.status(401).json({ message: "Not Allowed" });
+      } else {
+        console.log(`inside else`)
+
+        req.jwt = decodedToken;
+        console.log(req.jwt)
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "No token!" });
+  }
 };
