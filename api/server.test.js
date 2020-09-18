89cb8e2f6bcc43210644ea687a605d995679c0fa
royeradames <royeraadames@gmail.com>
2020-09-18 14:03:35 -0400

    1 test is working

--- api/server.test.js
+++ api/server.test.js
@@ -3,77 +3,58 @@ const supertest = require("supertest")
 const server = require("./server")
 const db = require("../database/dbConfig")
 
+
+let token
+
 describe("server", () => {
     describe("environment", () => {
         it('should set the DB_ENV variable to "testing"', () => {
             expect(process.env.DB_ENV).toBe("testing")
         })
     })
-    
-    describe("POST /hobbits", () => {
+
+    describe("Auth", () => {
         beforeEach(async () => {
             // trucate or empty the hobbits table
-            await db("hobbits").truncate()
+            await db("users").truncate()
         })
-
-        it("should return 201 when passed correct data", () => {
-            return supertest(server)
-                .post("/hobbits")
-                .send({ name: "sam" })
-                .then(res => {
-                    expect(res.status).toBe(201)
-                })
-        })
-
-        it("should fail with code 400 if passed incorrect data", () => {
-            return supertest(server)
-                .post("/hobbits")
-                .send({})
-                .then(res => {
-                    expect(res.status).toBe(400)
-                })
-        })
-
-        it("should insert the hobbit into the database", async () => {
-            const res = await supertest(server).post("/hobbits").send({ name: "sam" })
-
-            expect(res.body.data.name).toBe("sam")
-        })
-
-        it("should insert a collection of hobbits into the database", async () => {
-            const data = [
-                {
-                    name: "sam",
-                },
-                {
-                    name: "frodo",
-                },
-            ]
-
-            await supertest(server).post("/hobbits").send(data)
-
-            const hobbits = await db("hobbits")
-
-            expect(hobbits).toHaveLength(2)
-        })
-    })
-
-    describe("Users GET /", () => {
-        it("should return HTTP status code 200", () => {
-            return supertest(server)
-                .get("/")
-                .then(res => {
-                    expect(res.status).toBe(200)
-                })
-        })
-
-        it("should return JSON", async () => {
-            const res = await supertest(server).get("/")
-
-            expect(res.type).toMatch(/json/i)
-        })
-
         
+        describe('auth-router', () => {
+            describe('post /register', () => {
+                it("should return 201 when passed correct data", () => {
+                    return supertest(server)
+                        .post("/api/auth/register")
+                        .send({ username: "lambda", password: 'testing123' })
+                        .then(res => {
+                            expect(res.status).toBe(201)
+                            
+                            token = res.body.token
+                            console.log(token)
+                        })
+                })
+                it()
+            })
+        })
+
+
     })
-    
+
+    // describe("Users GET /", () => {
+    //     it("should return HTTP status code 200", () => {
+    //         return supertest(server)
+    //             .get("/")
+    //             .then(res => {
+    //                 expect(res.status).toBe(200)
+    //             })
+    //     })
+
+    //     it("should return JSON", async () => {
+    //         const res = await supertest(server).get("/")
+
+    //         expect(res.type).toMatch(/json/i)
+    //     })
+
+
+    // })
+
 })
