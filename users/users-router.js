c04351348cfd07796916f543d3c95a94fc754ef2
royeradames <royeraadames@gmail.com>
2020-09-18 12:44:08 -0400

    finish users-router v1. Working on Users model

---
+++ users/users-router.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+
+const Users = require("./users-model.js");
+const restricted = require("../auth/authenticate-middleware");
+
+router.get("/", restricted, (req, res) => {
+  console.log('users get /')
+  console.log(req.jwt)
+  console.log(req.jwt.department)
+  const department = { department: req.jwt.department }
+  Users.find(department)
+    .then(users => {
+      console.log(`inside findBy`)
+      console.log(users)
+      if (users.length) {
+        res.status(200).json(users);
+      } else {
+        res.status(200).json({message: 'no users at the moment'});
+      }
+    })
+    .catch(err => res.send(err.message));
+});
+
+
+
+module.exports = router;
