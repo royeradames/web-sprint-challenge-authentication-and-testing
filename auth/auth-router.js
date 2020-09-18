fff6362701aad08a5fee7e0b9cc8534f9ed773f9
royeradames <royeraadames@gmail.com>
2020-09-18 12:31:45 -0400

    completed v1 of /login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,31 +1,53 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
 router.post('/register', (req, res) => {
   // implement registration
-  const credentials = req.body;
+  const credentials = req.body
   if (isValid(credentials)) {
-    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const rounds = process.env.BCRYPT_ROUNDS || 8
     // hash the password
-    const hash = bcryptjs.hashSync(credentials.password, rounds);
-    credentials.password = hash;
+    const hash = bcryptjs.hashSync(credentials.password, rounds)
+    credentials.password = hash
     // save the user to the database
     Users.add(credentials)
       .then(user => {
-        const token = makeJwt(user);
-        res.status(201).json({ data: user, token });
+        const token = makeJwt(user)
+        res.status(201).json({ data: user, token })
       })
       .catch(error => {
-        res.status(500).json({ message: error.message });
-      });
+        res.status(500).json({ message: error.message })
+      })
   } else {
     res.status(400).json({
       message: "please provide username and password and the password shoud be alphanumeric",
-    });
+    })
   }
-});
+})
 
 router.post('/login', (req, res) => {
   // implement login
-});
+  const { username, password } = req.body
+  console.log(`users post /login`)
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        console.log(user)
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = makeJwt(user)
+          res.status(200).json({ token })
+        } else {
+          res.status(401).json({ message: "Invalid credentials" })
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message })
+      })
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    })
+  }
+})
 
-module.exports = router;
+module.exports = router
